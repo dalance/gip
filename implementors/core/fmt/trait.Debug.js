b25ebdb49dbbe1b4f9a7ea50622b981c6bf7be6a
dalance <dalance@gmail.com>
2016-02-23 00:11:48 +0900

    update doc

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gip'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='gip/enum.ProviderType.html' title='gip::ProviderType'>ProviderType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
