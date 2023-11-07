7554fa19d1870e48c84b32dcb76f0afcd146289b
dalance <dalance@gmail.com>
2023-11-07 09:07:18 +0900

    Remove unstable tests

--- src/lib.rs
+++ src/lib.rs
@@ -493,7 +493,7 @@ impl Provider for ProviderAny {
 /// ```
 /// use gip::{Provider, ProviderInfo};
 /// let mut p = ProviderInfo::new()
-///     .url("http://inet-ip.info/ip")
+///     .url("http://v4.ipv6-test.com/api/myip.php")
 ///     .create();
 /// let addr = p.get_addr().unwrap();
 /// println!( "{:?}", addr.v4addr );
@@ -948,34 +948,6 @@ impl Provider for ProviderDefaultV6 {
 #[cfg(test)]
 mod tests_v4 {
     use super::*;
-
-    //#[test]
-    //fn inet_ip() {
-    //    let mut p = ProviderInfo::new()
-    //        .name("inet-ip.info")
-    //        .ptype(ProviderInfoType::IPv4)
-    //        .protocol(ProviderInfoProtocol::HttpPlane)
-    //        .url("http://inet-ip.info/ip")
-    //        .create();
-    //    p.set_timeout(2000);
-    //    let addr = p.get_addr().unwrap();
-    //    assert!(addr.v4addr.is_some());
-    //    assert!(!addr.v4addr.unwrap().is_private());
-    //}
-
-    //#[test]
-    //fn ipify() {
-    //    let mut p = ProviderInfo::new()
-    //        .name("ipify.org")
-    //        .ptype(ProviderInfoType::IPv4)
-    //        .protocol(ProviderInfoProtocol::HttpPlane)
-    //        .url("http://api.ipify.org")
-    //        .create();
-    //    p.set_timeout(2000);
-    //    let addr = p.get_addr().unwrap();
-    //    assert!(addr.v4addr.is_some());
-    //    assert!(!addr.v4addr.unwrap().is_private());
-    //}
 
     #[test]
     fn ipv6_test() {
